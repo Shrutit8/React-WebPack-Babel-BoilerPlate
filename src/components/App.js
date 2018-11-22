8beff0711cc37e0f8c4a201df5526964271e570e
Shrutit8 <shrutit8@gmail.com>
2018-11-22 14:32:36 +0100

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import '../styles/App.css';
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <h1>React BoilerPlate Starters Kit</h1>
+                <p>With every new project that you start, you will be repeating yourself creating boilerplate. 
+                    You could also introduce inconsistencies in how these libraries are set up in each project.</p>
+                <p> This can cause confusion when you switch between projects.
+                    This is where boilerplates come in. </p>
+                <p>A boilerplate is a template that you can clone and reuse for every project.</p>
+                </div>
+        )
+    }
+}
+
+export default App;
